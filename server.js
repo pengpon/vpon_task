cbbd08e967384e51378ea3183e27f7819c742522
DESKTOP-1REJH32\User <bd102pon@gmail.com>
2019-10-30 17:59:40 +0800

    no message

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-//import express 和 ws 套件
-const express = require('express')
-const SocketServer = require('ws').Server
+'use strict';
 
-//指定開啟的 port
-// const PORT = 3000
-const port = process.env.PORT || 3000;
-//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
 const server = express()
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+    .use((req, res) => res.sendFile(INDEX))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-//將 express 交給 SocketServer 開啟 WebSocket 的服務
 const wss = new SocketServer({
     server
-})
+});
 
 //當 WebSocket 從外部連結時執行
 wss.on('connection', ws => {
