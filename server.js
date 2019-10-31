3e99a7872b610ab48480c8bf38a6d7ba39de67e9
DESKTOP-1REJH32\User <bd102pon@gmail.com>
2019-10-31 16:14:49 +0800

    no message

--- server.js
+++ server.js
@@ -15,18 +15,21 @@ const wss = new SocketServer({
     server
 });
 
+
 //當 WebSocket 從外部連結時執行
 wss.on('connection', ws => {
-
     //連結時執行此 console 提示
     console.log('Client connected')
+
+    let clients = wss.clients;
+    console.log(clients.size);
 
     //對 message 設定監聽，接收從 Client 發送的訊息
     ws.on('message', data => {
         //取得所有連接中的 client
-        let clients = wss.clients
+        let clients = wss.clients;
 
-        //做迴圈，發送訊息至每個 client
+        //發送訊息至每個 client
         clients.forEach(client => {
             client.send(data)
         })
