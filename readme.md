b499f9cbdd4e177b2c9cd8df3672f86859b54040
DESKTOP-1REJH32\User <bd102pon@gmail.com>
2019-11-11 16:43:04 +0800

    no message

---
+++ readme.md
@@ -0,0 +1,20 @@
+# 執行步驟
+
+1. 本機run server.js(設定使用3000 port)
+
+   > npm start
+
+![sever run](https://imgur.com/iXDDi4N.jpg)
+
+1. 利用網頁server(ex:prepros)，開啟index.html
+   - 畫面提醒 另使用手機裝置google browser 開啟此連結
+
+![index page](https://imgur.com/BIpSO0d.jpg)
+
+3. 手機裝置必須與本機使用相同網路，於手機google browser輸入server ip:port (ex:192.168.1.31:7788)
+
+![mobile page](https://imgur.com/VyrxYet.jpg)
+
+4. 搖動手機裝置，桌機or筆電的網頁畫面則會顯示結果
+
+   ![Result](https://imgur.com/q3GGdnz.jpg)
--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-const path = require('path');
+// const path = require('path');
 // var http = require('http');
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+// const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX))
+    // .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // var wss = http.createServer();
